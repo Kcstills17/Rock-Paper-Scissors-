56b61f48be5d95db640eb5644a50c8d77251a0bd
kcstills17 <zanelee28@yahoo.comm>
2022-07-13 14:29:24 -0400

    removed UserPlay, was not neccessary. created a playRound function. set parameters to their respective player. and created a game function to execute all together.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,29 +1,47 @@
 // Starting a Rock-Paper-Scissors Project //
 
+  let playRound = function(playerSelection, computerSelection) {
+      if (playerSelection == "Rock" && computerSelection == "Rock") {
+          return "Both choose Rock. It is a tie!";
+      }  else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+          return "You win! Rock beats Scissors!";
+      }  else if (playerSelection == "Rock" && computerSelection == "Paper") {
+          return "You lose! Paper beats Rock.";
+      }   else if (playerSelection == "Paper" && computerSelection == "Rock"){
+          return "You win! Paper beats Rock!";
+      }  else if (playerSelection == "Paper" && computerSelection == "Paper"){
+        return "Both choose Paper. It is a tie!";
+      } else if (playerSelection == "Paper" && computerSelection == "Scissors"){
+        return "You Lose! Scissors beats Paper.";
+      } else if (playerSelection == "Scissors" && computerSelection == "Rock"){
+        return "You Lose! Rock beats Scissors.";
+      } else if (playerSelection == "Scissors" && computerSelection == "Scissors"){
+        return "Both choose Scissors! It is a tie!.";
+      } else if (playerSelection == "Scissors" && computerSelection == "Paper"){
+        return "You win! Scissors beats Paper."; 
+      }  else {
+          return "not a valid choice. Please choose agaain"
+      }
+
+  }
 let computerPlay = function () {
-    let choices = ['rock', 'paper', 'scissors'];
+    let choices = ['Rock', 'Paper', 'Scissors'];
 
 
 let randomChoice = choices[Math.floor(Math.random() * choices.length)];
- return (`Your opponent chooses ${randomChoice}`);
+ return (`${randomChoice}`);
 }
 
+ 
+const playerSelection = prompt("Rock, Papers, or Scissors?")
 
-let userPlay = function () {
-    let yourAnswer = ('Rock, Paper, or Scissors?')
+const computerSelection = computerPlay();
+//console.log(playRound(playerSelection, computerSelection));
+
+ let game = function () {
+     return (playRound(playerSelection, computerSelection));
+
+ }
+ game();
+
    
-    if(yourAnswer.toLowerCase() == "rock") {
-        return ("you chose rock");
-    } if(yourAnswer.toLowerCase() == "paper") {
-        return ("you chose paper");
-    } if(yourAnswer.toLowerCase() == "scissors") {
-        return ("you chose scissors");
-    } else {
-        return ("Not rock, paper, or scissors. Please choose again?")
-    }
-    
-} 
-
-    let playRound = function(playerSelection, userSelection) {
-        
-    }
