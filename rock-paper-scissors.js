d0384111a2a0e4292e81a405c950f9fee8bbd3d5
kcstills17 <zanelee28@yahoo.comm>
2022-07-14 14:32:29 -0400

    changed function names to fit problem criteria. Created game function that loops 5 times. Fixed computerSelection not being random by inserting it within game function. Now need to give player and cpu points on every round and declare the winner of each game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,28 +1,30 @@
 // Starting a Rock-Paper-Scissors Project //
+let playerScore = 0; 
+let computerScore = 0; 
 
   let playRound = function(playerSelection, computerSelection) {
       if (playerSelection == "Rock" && computerSelection == "Rock") {
-          return "Both choose Rock. It is a tie!";
+          return ("Both choose Rock. It is a tie!");
       }  else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-          return "You win! Rock beats Scissors!";
+          return ("You win! Rock beats Scissors!");
       }  else if (playerSelection == "Rock" && computerSelection == "Paper") {
-          return "You lose! Paper beats Rock.";
+          return ("You lose! Paper beats Rock.");
       }   else if (playerSelection == "Paper" && computerSelection == "Rock"){
-          return "You win! Paper beats Rock!";
+          return ("You win! Paper beats Rock!");
       }  else if (playerSelection == "Paper" && computerSelection == "Paper"){
-        return "Both choose Paper. It is a tie!";
+        return ("Both choose Paper. It is a tie!");
       } else if (playerSelection == "Paper" && computerSelection == "Scissors"){
-        return "You Lose! Scissors beats Paper.";
+        return ("You Lose! Scissors beats Paper.");
       } else if (playerSelection == "Scissors" && computerSelection == "Rock"){
-        return "You Lose! Rock beats Scissors.";
+        return ("You Lose! Rock beats Scissors.");
       } else if (playerSelection == "Scissors" && computerSelection == "Scissors"){
-        return "Both choose Scissors! It is a tie!.";
+        return ("Both choose Scissors! It is a tie!.");
       } else if (playerSelection == "Scissors" && computerSelection == "Paper"){
-        return "You win! Scissors beats Paper."; 
+        return ("You win! Scissors beats Paper."); 
       }  else {
-          return "not a valid choice. Please choose agaain"
+          return ("not a valid choice. Please choose again")
       }
-
+    
   }
 let computerPlay = function () {
     let choices = ['Rock', 'Paper', 'Scissors'];
@@ -36,12 +38,33 @@ let randomChoice = choices[Math.floor(Math.random() * choices.length)];
 const playerSelection = prompt("Rock, Papers, or Scissors?")
 
 const computerSelection = computerPlay();
-//console.log(playRound(playerSelection, computerSelection));
 
- let game = function () {
-     return (playRound(playerSelection, computerSelection));
+   
 
- }
- game();
+const game = function() {
+ 
+  for (let i = 0; i < 5; i++) 
+  {
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+
+  if (playerSelection.toLowerCase == "Rock" && computerSelection.toLowerCase == "Paper") {
+    return computerScore++;
+  } if (playerSelection.toLowerCase == "Rock" && computerSelection.toLowerCase == "Scissors") {
+    return playerScore++;
+  } if (playerSelection.toLowerCase == "Paper" && computerSelection.toLowerCase == "Scissors") {
+    return computerScore++;
+  } if (playerSelection.toLowerCase == "Paper" && computerSelection.toLowerCase == "Rock") {
+    return playerScore++;
+  } if (playerSelection.toLowerCase == "Scissors" && computerSelection.toLowerCase == "Rock") {
+    return computerScore++;
+  } if (playerSelection.toLowerCase == "Scissors" && computerSelection.toLowerCase == "Paper") {
+    return computerScore++;
+  }
+}
+console.log(game());
+ 
+ 
 
    
